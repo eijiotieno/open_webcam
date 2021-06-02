94f2559a0656cc75469321696d148cd411a9bd6a
eijiotieno <61346906+eijiotieno@users.noreply.github.com>
2021-06-02 09:10:10 +0300

    Add files via upload

---
+++ webcam/webcam.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+camera = cv2.VideoCapture(0)
+
+while True:
+    val, frame = camera.read()
+    cv2.imshow("frame", frame)
+
+    if cv2.waitKey(1) == ord("e"):
+        break
+
+
+camera.release()
+cv2.destroyAllWindows()
